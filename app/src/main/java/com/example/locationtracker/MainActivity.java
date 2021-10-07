65d352826038b10166539887d05ea3232aa53255
caitlinmorris <cemorris3@wisc.edu>
2021-10-07 14:52:54 -0500

    The onRequestPermissionsResult() method for processing permission results

--- app/src/main/java/com/example/locationtracker/MainActivity.java
+++ app/src/main/java/com/example/locationtracker/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.locationtracker;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,9 +31,6 @@ public class MainActivity extends AppCompatActivity {
                 updateLocationInfo(location);
             }
 
-            private void updateLocationInfo(Location location) {
-            }
-
             @Override
             public void onStatusChanged(String s, int i, Bundle bundle){
 
@@ -42,5 +44,40 @@ public class MainActivity extends AppCompatActivity {
 
             }
         };
+
+        if(Build.VERSION.SDK_INT < 23){
+            // this method makes our app start listening to the location, we will define it later
+            startListening();
+        } else {
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            } else {
+
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if(location != null){
+                    // this method is used to update the location info on the screen, we will define it later
+                    updateLocationInfo(location);
+                }
+            }
+        }
+    }
+
+    public void startListening(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        }
+    }
+
+    private void updateLocationInfo(Location location) {
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if ( grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            startListening();
+        }
     }
 }
